311fc3a216fe841be613a94949154aef4c4b0839
elmurd0r <gameralc@web.de>
2020-10-07 00:06:02 +0200

    Add title component

---
+++ src/Title.es.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Title = () => {
+    return (
+        <div className="row justify-content-md-center mb-4 h-50">
+            <div className="col-md-6 align-self-end">
+                <h1 className="display-1 text-center">Dynamic BPMN Viewer</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Title;
