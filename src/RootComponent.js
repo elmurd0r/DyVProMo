8c5959ad2c9df27908e06ec9f634b173a07119a3
dab0bby <gennadi@eirich.io>
2022-08-14 21:56:54 +0200

    Adds support for static model file

--- src/RootComponent.js
+++ src/RootComponent.js
@@ -4,18 +4,36 @@ import BpmnViewer from "./viewer/BpmnViewer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import FileImport from "./fileimport/FileImport";
 import Title from "./Title";
+import Viewer from "bpmn-js";
+import bpmnFile from "./files/SupportablaufCostumerRequest.bpmn";
 
 const RootComponent = () => {
     const [fileData, setFileData] = useState(null);
 
+    fetch(bpmnFile)
+        .then((response) => response.text())
+        .then((content) => {
+            let view = new Viewer();
+            view.importXML(content).then(
+                () => {
+                    setFileData(content);
+                },
+                (onReject) => {
+                    console.error("Failed to load static XML-File. Error:", onReject);
+                }
+            );
+        });
+
     if (!fileData) {
-        return (<div className="container h-100">
-            <Title />
-            <FileImport setFileData={setFileData} />
-        </div>);
+        return (
+            <div className="container h-100">
+                <Title />
+                <FileImport setFileData={setFileData} />
+            </div>
+        );
     }
 
-    return (<BpmnViewer fileData={fileData} setFileData={setFileData} />);
+    return <BpmnViewer fileData={fileData} setFileData={setFileData} />;
 };
 
 export default RootComponent;
